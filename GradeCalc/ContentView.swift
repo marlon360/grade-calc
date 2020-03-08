42fa36b6d2f94e5b8cee8601ab0c9be57ccca804
Marlon Lückert <m.lueckert@me.com>
2020-03-08 22:05:23 +0100

    Initial Commit

---
+++ GradeCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GradeCalc
+//
+//  Created by Marlon Lückert on 08.03.20.
+//  Copyright © 2020 Marlon Lückert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
