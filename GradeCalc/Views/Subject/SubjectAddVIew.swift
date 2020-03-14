ca24f911d0a02428a031bf9317c47bc8fe752681
Marlon Lückert <m.lueckert@me.com>
2020-03-14 18:37:58 +0100

    move view up when keyboard active

--- GradeCalc/Views/Subject/SubjectAddVIew.swift
+++ GradeCalc/Views/Subject/SubjectAddVIew.swift
@@ -9,6 +9,31 @@
 import Foundation
 import SwiftUI
 
+final class KeyboardResponder: ObservableObject {
+    private var notificationCenter: NotificationCenter
+    @Published private(set) var currentHeight: CGFloat = 0
+
+    init(center: NotificationCenter = .default) {
+        notificationCenter = center
+        notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    deinit {
+        notificationCenter.removeObserver(self)
+    }
+
+    @objc func keyBoardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            currentHeight = keyboardSize.height
+        }
+    }
+
+    @objc func keyBoardWillHide(notification: Notification) {
+        currentHeight = 0
+    }
+}
+
 struct SubjectAddView: View {
     
     @State var subject: Subject?
@@ -35,6 +60,8 @@ struct SubjectAddView: View {
     
     @State private var semesterChooser = 0
     @State private var simulationChooser: Int
+    
+    @ObservedObject private var keyboard = KeyboardResponder()
     
     init(subject bindedSubject: Subject?, isPresented: Binding<Bool>) {
         self._subject = State(initialValue: bindedSubject)
@@ -178,6 +205,9 @@ struct SubjectAddView: View {
                     Text("Abbrechen")
                 }
             )
+            .padding(.bottom, keyboard.currentHeight)
+            .edgesIgnoringSafeArea(.bottom)
+            .animation(.easeOut(duration: 0.16))
         }
     }
     
